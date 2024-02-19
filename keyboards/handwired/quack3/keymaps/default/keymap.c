5682022530fa6fc6ce4d129daf4d86208f463e63
Leonidas Stratigakis <leo.stratigakis@gmail.com>
2024-02-20 00:00:01 +0200

    quack3 and led test

--- keyboards/handwired/quack3/keymaps/default/keymap.c
+++ keyboards/handwired/quack3/keymaps/default/keymap.c
@@ -5,8 +5,13 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 <<<<<<< HEAD
+<<<<<<< HEAD
     [0] = LAYOUT_7x14(
         KC_AUDIO_MUTE, KC_CALCULATOR,
+=======
+    [0] = LAYOUT_7x14(
+        KC_CALCULATOR,
+>>>>>>> 87b973da14 (quack3 and led test)
         KC_ESCAPE, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
         KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BACKSPACE,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_BACKSLASH,
@@ -15,7 +20,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LEFT_CTRL, KC_LEFT_GUI, KC_LEFT_ALT, KC_SPACE, MO(1), KC_RIGHT_ALT, MO(1), KC_RIGHT_CTRL
     ),
     [1] = LAYOUT_7x14(
+<<<<<<< HEAD
         KC_AUDIO_MUTE, KC_CALCULATOR,
+=======
+        KC_CALCULATOR,
+>>>>>>> 87b973da14 (quack3 and led test)
         KC_ESCAPE, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
         KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_DELETE,
         KC_DELETE, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_UP, KC_O, KC_PAGE_UP, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_BACKSLASH,
@@ -23,6 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LEFT_SHIFT, KC_Z, KC_X, KC_C, KC_END, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RIGHT_SHIFT,
         KC_LEFT_CTRL, KC_LEFT_GUI, KC_LEFT_ALT, KC_SPACE, KC_NO, KC_RIGHT_ALT, KC_NO, KC_RIGHT_CTRL
     ),
+<<<<<<< HEAD
 };
 
 
@@ -52,3 +62,14 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     )
 };
 >>>>>>> 3340c010df (quack2)
+=======
+};
+
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [1] =   { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN) },
+};
+#endif
+>>>>>>> 87b973da14 (quack3 and led test)
--- keyboards/handwired/quack3/rules.mk
+++ keyboards/handwired/quack3/rules.mk
@@ -1,5 +1,9 @@
 # This file intentionally left blank
 <<<<<<< HEAD
+<<<<<<< HEAD
 ENCODER_MAP_ENABLE = yes
 =======
 >>>>>>> 3340c010df (quack2)
+=======
+ENCODER_MAP_ENABLE = yes
+>>>>>>> 87b973da14 (quack3 and led test)
