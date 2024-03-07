6464c92efef21f25a59987058d09b5d88798985e
Leonidas Stratigakis <leo.stratigakis@gmail.com>
2024-03-07 23:16:05 +0200

    quackshift (testing shift register)

---
+++ keyboards/handwired/quackshift/keymaps/default/keymap.c
@@ -0,0 +1,13 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_1, KC_2, KC_3, KC_4,
+        KC_Q, KC_W, KC_E, KC_R,
+        KC_A, KC_S, KC_D, KC_F,
+        KC_Z, KC_X, KC_C, KC_V
+    )
+};
