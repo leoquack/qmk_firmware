a4a0f699af51945a9034a9ad70d198a8f00689e7
Leonidas Stratigakis <leo.stratigakis@gmail.com>
2024-03-08 21:38:46 +0200

    minor changes

--- keyboards/handwired/quackshift/config.h
+++ keyboards/handwired/quackshift/config.h
@@ -22,9 +22,9 @@
 // Define your HC595 pin mappings
 #define SPI_DRIVER SPID0
 #define SPI_MOSI_PIN GP0
-#define SPI_SCK_PIN GP1
-#define SPI_LATCH_PIN GP2
-#define SPI_DIVISOR 32
+#define SPI_SCK_PIN GP2
+#define SPI_CS_PIN GP1
+#define SPI_DIVISOR 16
 #define SPI_MODE 3
 #define SPI_lsbFirst true
 
@@ -32,7 +32,7 @@
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 4
 
-#define ROWS {GP6, GP7, GP8, GP9}
+#define ROWS { GP6, GP7, GP8, GP9 }
 //0x0001, 0x0002, 0x0004, 0x0008, 0x0010, 0x0020, 0x0040, 0x0080, 0x0100, 0x0200, 0x0400, 0x0800, 0x1000, 0x2000, 0x4000, 0x8000
 // 1QA     1QB     1QC     1QD     1QE     1QF     1QG     1QH     2QA     2QB     2QC     2QD     2QE     2QF     2QG     2QH
 #define COLS { 0x0002, 0x0004, 0x0008, 0x0010 }
