14037edb9bf589548c0c0b58b90d13dc8f20c911
Leonidas Stratigakis <leo.stratigakis@gmail.com>
2024-03-16 19:42:26 +0200

    revert rows

--- keyboards/handwired/quackshift/config.h
+++ keyboards/handwired/quackshift/config.h
@@ -34,10 +34,10 @@
 
 // Hexadecimal:  0    1    2    3    4    5    6    7    8    9    A    B    C    D    E    F
 // Binary:    0000 0001 0010 0011 0100 0101 0110 0111 1000 1001 1010 1011 1100 1101 1110 1111
-// #define ROWS { GP6, GP7, GP8, GP9 }
+#define ROWS { GP6, GP7, GP8, GP9 }
 //0x0001, 0x0002, 0x0004, 0x0008, 0x0010, 0x0020, 0x0040, 0x0080, 0x0100, 0x0200, 0x0400, 0x0800, 0x1000, 0x2000, 0x4000, 0x8000
 // 1QA     1QB     1QC     1QD     1QE     1QF     1QG     1QH     2QA     2QB     2QC     2QD     2QE     2QF     2QG     2QH
-#define ROWS { 0x0002, 0x0004, 0x0008, 0x0010 }
+// #define ROWS { 0x0002, 0x0004, 0x0008, 0x0010 }
 #define COLS { 0x0002, 0x0004, 0x0008, 0x0010 }
 
 
