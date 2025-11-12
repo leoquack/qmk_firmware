b4d4e5895fd7e31501fdf9275eff3d96b1deb842
Leonidas Stratigakis <leo.stratigakis@gmail.com>
2025-11-12 15:14:28 +0200

    fix all quack keyboards to compile (except quack1,2 and 3)

--- keyboards/handwired/quack3/rules.mk
+++ keyboards/handwired/quack3/rules.mk
@@ -1,9 +1,2 @@
 # This file intentionally left blank
-<<<<<<< HEAD
-<<<<<<< HEAD
 ENCODER_MAP_ENABLE = yes
-=======
->>>>>>> 3340c010df (quack2)
-=======
-ENCODER_MAP_ENABLE = yes
->>>>>>> 87b973da14 (quack3 and led test)
